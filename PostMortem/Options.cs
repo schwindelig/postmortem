a88fb52b7e8337dd92a31c8816a2ba15d84cbaf8
David Szöke <david.szoeke@unic.com>
2019-03-02 07:54:01 +0100

    Add command line options

---
+++ PostMortem/Options.cs
@@ -0,0 +1,20 @@
+﻿using CommandLine;
+
+namespace PostMortem
+{
+    public class Options
+    {
+        [Option(
+            'p', "path",
+            HelpText = "Path to the memory dump",
+            Required = true)]
+        public string Path { get; set; }
+
+        [Option(
+            'v', "verbose",
+            HelpText = "Verbose output",
+            Required = false,
+            Default = false)]
+        public bool Verbose { get; set; }
+    }
+}
---
+++ PostMortem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PostMortem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandLine.Parser.Default.ParseArguments<Options>(args);
+        }
+    }
+}
