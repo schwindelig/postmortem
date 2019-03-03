13a270f5a188ee49e34f4a711f06164e14a565a4
David Szöke <david.szoeke@unic.com>
2019-03-03 08:11:08 +0100

    Make Markdown project target .net standard 2.0, add reporter class

---
+++ PostMortem.Core/Report/Reporter.cs
@@ -0,0 +1,13 @@
+﻿using PostMortem.Core.Results;
+using PostMortem.Markdown;
+
+namespace PostMortem.Core.Report
+{
+    public class Reporter
+    {
+        public void GenerateReport(AnalysisResult result)
+        {
+            var document = new MarkdownDocument();
+        }
+    }
+}
